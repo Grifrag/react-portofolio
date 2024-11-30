787842a69b4a15fcb0261f351db97b968660de3f
Grigoris Marios Fragkakis <66517447+Grifrag@users.noreply.github.com>
2024-11-30 03:17:55 +0200

    Update Footer.jsx

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
         <SocialMediaIcons />
         <div className="md:flex justify-center md:justify-between text-center ">
           <p className="font-playfair font-semibold text-2xl text-yellow">
-            Grigoris Fragkakis
+            Grigorios Fragkakis
           </p>
           <p className="font-playfair text-md text-yellow">
-            ©2022 FRAGKAKIS. All Rights Reserved.
+            ©2022 Grigorios Fragkakis. All Rights Reserved.
           </p>
         </div>
       </div>
